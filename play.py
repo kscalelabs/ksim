c4431644df148437b2c0a47253dc769c96a6f507
Michael Lutz <michaeljeffreylutz@gmail.com>
2024-05-22 15:33:58 -0700

    feat: printing when rendering

--- play.py
+++ play.py
@@ -5,11 +5,14 @@ from envs import get_env
 from brax.io import model
 from brax.training.agents.ppo import networks as ppo_networks
 import mediapy as media
+import numpy as np
+from tqdm import tqdm
 import jax
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='Run PPO training with specified config file.')
 parser.add_argument('--config', type=str, required=True, help='Path to the config YAML file')
+parser.add_argument('--use_mujoco', type=bool, default=False, help='Use mujoco instead of mjx for rendering')
 args = parser.parse_args()
 
 # Load config from YAML file
@@ -43,6 +46,7 @@ env = get_env(
     exclude_current_positions_from_observation=exclude_current_positions_from_observation,
     log_reward_breakdown=log_reward_breakdown
 )
+print(f'Loaded environment with env.observation_size: {env.observation_size} and env.action_size: {env.action_size}')
 
 # Loading params
 model_path = "weights/" + config.get('project_name', '') + ".pkl"
@@ -52,6 +56,7 @@ inference_fn = ppo_networks.make_inference_fn(policy_network)(params)
 jit_inference_fn = jax.jit(inference_fn)
 jit_reset = jax.jit(env.reset)
 jit_step = jax.jit(env.step)
+print(f'Loaded params from {model_path}')
 
 # initialize the state
 rng = jax.random.PRNGKey(0)
@@ -59,16 +64,33 @@ state = jit_reset(rng)
 rollout = [state.pipeline_state]
 
 # grab a trajectory
-n_steps = 100
+n_steps = 1000
 render_every = 2
 
-for i in range(n_steps):
+for i in tqdm(range(n_steps)):
   act_rng, rng = jax.random.split(rng)
   ctrl, _ = jit_inference_fn(state.obs, act_rng)
   state = jit_step(state, ctrl)
   rollout.append(state.pipeline_state)
 
-  if state.done:
-    break
+print(f'Rolled out {len(rollout)} steps')
 
-media.show_video(env.render(rollout[::render_every], camera='side'), fps=1.0 / env.dt / render_every)
+images = env.render(rollout[::render_every], camera='side')
+print(f'Generated {len(images)} images')
+try:
+    print(images[0].shape)
+except:
+    print(images[0].size)
+
+images_thwc = np.array(images)
+images_tchw = np.transpose(images_thwc, (0, 3, 1, 2))
+
+try:
+    print(images_tchw.shape)
+except:
+    print(images_tchw.size)
+
+fps = 1/env.dt
+wandb.log({'training_rollouts': wandb.Video(images_tchw, fps=fps, format="mp4")})
+
+media.write_video('video.mp4', images_thwc, fps=fps)
