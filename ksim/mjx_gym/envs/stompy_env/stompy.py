9703f462254c463acbc4380b7f7b00b1068d3118
Michael Lutz <michaeljeffreylutz@gmail.com>
2024-05-29 05:10:23 +0000

    feat: stompy begins to walk

--- ksim/mjx_gym/envs/stompy_env/stompy.py
+++ ksim/mjx_gym/envs/stompy_env/stompy.py
@@ -30,7 +30,7 @@ class StompyEnv(PipelineEnv):
         log_reward_breakdown: bool = True,
         **kwargs: Unpack[EnvKwargs],
     ) -> None:
-        path = os.getenv("MODEL_DIR", "") + "/h1_scene.xml"
+        path = os.getenv("MODEL_DIR", "") + "/stompy/stompy.xml"
         mj_model = mujoco.MjModel.from_xml_path(path)
         mj_model.opt.solver = mujoco.mjtSolver.mjSOL_CG
         mj_model.opt.iterations = 6
