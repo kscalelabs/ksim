12e38a4d76903ceb5ca6ab3ee26a6ad57955abcc
Michael Lutz <michaeljeffreylutz@gmail.com>
2025-02-21 15:56:12 -0800

    feat: basic MLP with somewhat standard init

---
+++ ksim/model/mlp.py
@@ -0,0 +1,35 @@
+"""Base MLP implementation using Flax Linen."""
+
+import flax.linen as nn
+import jax
+
+
+class MLP(nn.Module):
+    """A multi-layer perceptron."""
+
+    num_hidden_layers: int
+    """The number of hidden layers in the MLP."""
+
+    hidden_features: int
+    """The number of features in the hidden layers."""
+
+    out_features: int
+    """The number of features in the output layer."""
+
+    @nn.compact
+    def __call__(self, x: jax.Array) -> jax.Array:
+        """Forward pass of the MLP.
+
+        Args:
+            x: The input to the MLP [..., in_features]
+
+        Returns:
+            The output of the MLP [..., out_features]
+        """
+        for _ in range(self.num_hidden_layers):
+            x = nn.Dense(
+                features=self.hidden_features, kernel_init=nn.initializers.kaiming_normal()
+            )(x)
+            x = nn.relu(x)
+        x = nn.Dense(features=self.out_features, kernel_init=nn.initializers.xavier_normal())(x)
+        return x
