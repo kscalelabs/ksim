2ff8333df88878db4e15f2881996fd924b87bde9
alik-git <ali@kscale.dev>
2025-02-26 19:43:58 +0000

    remove builders, simplify

--- ksim/env/builders/loggers.py
+++ ksim/env/builders/loggers.py
@@ -1,7 +1,7 @@
 """Defines a standard interface for logging metrics."""
 
 from abc import ABC, abstractmethod
-from typing import Generic, NamedTuple, Optional, TypeVar
+from typing import Generic, NamedTuple, TypeVar
 
 import attrs
 import jax.numpy as jnp
@@ -29,13 +29,13 @@ class TrajectoryData(NamedTuple):
 
 @attrs.define(frozen=True, kw_only=True)
 class LoggingData:
-    trajectory: Optional[TrajectoryData] = None
+    trajectory: TrajectoryData | None = None
     update_metrics: dict[str, jnp.ndarray] = attrs.field(
         factory=dict
-    )  # e.g., policy_loss, value_loss, entropy, total_loss
-    gradients: Optional[PyTree] = None
-    loss: Optional[float] = None
-    training_state: Optional[xax.State] = None
+    )
+    gradients: PyTree | None = None
+    loss: float | None = None
+    training_state: xax.State | None = None
 
 
 @attrs.define(frozen=True, kw_only=True)
@@ -52,85 +52,33 @@ class LogMetric(ABC):
         """Returns a human-friendly name for the metric."""
         return xax.camelcase_to_snakecase(self.__class__.__name__)
 
-
-T = TypeVar("T", bound=LogMetric)
-
-
-class LogMetricBuilder(ABC, Generic[T]):
-    @abstractmethod
-    def __call__(self, data: Optional[BuilderData] = None) -> T:
-        """Constructs and returns a log metric instance given some builder data."""
-
-
 @attrs.define(frozen=True, kw_only=True)
 class EpisodeLengthLog(LogMetric):
     def __call__(self, data: LoggingData) -> jnp.ndarray:
-        """Computes the average episode length."""
         if data.trajectory is None:
             raise ValueError("Trajectory cannot be None for EpisodeLengthLog")
         trajectory = data.trajectory
-        return jnp.sum(~trajectory.done) / (jnp.sum(trajectory.done) + 1)
+        # Ensure we have at least one termination event.
+        episode_count = jnp.sum(trajectory.done).clip(min=1)
+        return jnp.sum(~trajectory.done) / episode_count
 
-
-class EpisodeLengthLogBuilder(LogMetricBuilder[EpisodeLengthLog]):
-    def __init__(self) -> None:
-        pass
-
-    def __call__(self, data: Optional[BuilderData] = None) -> EpisodeLengthLog:
-        return EpisodeLengthLog()
-
-
+@attrs.define(frozen=True, kw_only=True)
 class AverageRewardLog(LogMetric):
     def __call__(self, data: LoggingData) -> jnp.ndarray:
-        """Computes the average reward per episode."""
         if data.trajectory is None:
             raise ValueError("Trajectory cannot be None for AverageRewardLog")
         trajectory = data.trajectory
         total_reward = jnp.sum(trajectory.rewards)
-        episode_count = jnp.sum(trajectory.done) + 1  # +1 to avoid division by zero
+        episode_count = jnp.sum(trajectory.done).clip(min=1)
         return total_reward / episode_count
 
 
-class AverageRewardLogBuilder(LogMetricBuilder[AverageRewardLog]):
-    def __call__(self, data: Optional[BuilderData] = None) -> AverageRewardLog:
-        return AverageRewardLog()
+@attrs.define(frozen=True)
+class ModelUpdateLog(LogMetric):
+    name: str = attrs.field()
 
-
-class PolicyLossLog(LogMetric):
     def __call__(self, data: LoggingData) -> jnp.ndarray:
-        return data.update_metrics["policy_loss"]
+        return data.update_metrics[self.name]
 
-
-class PolicyLossLogBuilder(LogMetricBuilder[PolicyLossLog]):
-    def __call__(self, data: Optional[BuilderData] = None) -> PolicyLossLog:
-        return PolicyLossLog()
-
-
-class ValueLossLog(LogMetric):
-    def __call__(self, data: LoggingData) -> jnp.ndarray:
-        return data.update_metrics["value_loss"]
-
-
-class ValueLossLogBuilder(LogMetricBuilder[ValueLossLog]):
-    def __call__(self, data: Optional[BuilderData] = None) -> ValueLossLog:
-        return ValueLossLog()
-
-
-class EntropyLog(LogMetric):
-    def __call__(self, data: LoggingData) -> jnp.ndarray:
-        return data.update_metrics["entropy"]
-
-
-class EntropyLogBuilder(LogMetricBuilder[EntropyLog]):
-    def __call__(self, data: Optional[BuilderData] = None) -> EntropyLog:
-        return EntropyLog()
-
-
-class TotalLossLog(LogMetric):
-    def __call__(self, data: LoggingData) -> jnp.ndarray:
-        return data.update_metrics["total_loss"]
-
-
-class TotalLossLogBuilder(LogMetricBuilder[TotalLossLog]):
-    def __call__(self, data: Optional[BuilderData] = None) -> TotalLossLog:
-        return TotalLossLog()
+    def get_name(self) -> str:
+        return self.name
