9a467148ed473e3c799784e55b4f2c98df2676c3
Wesley Maa <wesley.maa@gmail.com>
2025-03-11 04:02:01 +0000

    adjust height reward

--- examples/kbot/standing.py
+++ examples/kbot/standing.py
@@ -148,7 +148,7 @@ class KBotStandingTask(PPOTask[KBotStandingConfig]):
                 LinearVelocityZPenalty(scale=-0.0),
                 AngularVelocityXYPenalty(scale=-0.15),
                 TrackLinearVelocityXYReward(scale=1.0),
-                HeightReward(scale=1.0, height_target=1.0),
+                HeightReward(scale=5.0, height_target=0.98),
                 TrackAngularVelocityZReward(scale=1.0),
                 ActionSmoothnessPenalty(scale=-0.0),
                 OrientationPenalty(scale=-0.5, target_orientation=[0.0, 0.0, 0.0]),
