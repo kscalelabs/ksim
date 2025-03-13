e4579cf6cf19045b9fbd79b5a96db3d33bbcf2d0
Benjamin Bolte <ben@bolte.cc>
2025-03-13 23:41:21 +0800

    same file for actuators

--- ksim/actuators.py
+++ ksim/actuators.py
@@ -1,13 +1,30 @@
-"""MIT Controller, as used by the Robstride actuators."""
+"""Defines the base actuators class, along with some implementations."""
 
+from abc import ABC, abstractmethod
 from typing import Mapping, TypedDict
 
+import attrs
 import jax.numpy as jnp
 from jaxtyping import Array
 from mujoco import mjx
 
-from ksim.actuators.base import Actuators
 from ksim.utils.mujoco import MujocoMappings
+
+
+@attrs.define(frozen=True)
+class Actuators(ABC):
+    """Collection of actuators."""
+
+    @abstractmethod
+    def get_ctrl(self, mjx_data: mjx.Data, action: Array) -> Array:
+        """Get the control signal from the action vector."""
+        ...
+
+
+class TorqueActuators(Actuators):
+    def get_ctrl(self, mjx_data: mjx.Data, action: Array) -> Array:
+        # Just use the action as the torque, the simplest actuator model.
+        return action
 
 
 class PositionParams(TypedDict):
@@ -52,4 +69,29 @@ class MITPositionActuators(Actuators):
         vel_delta = target_velocities - current_vel
 
         ctrl = self.kps * pos_delta + self.kds * vel_delta
+        return ctrl
+
+
+class ScaledTorqueActuators(Actuators):
+    def __init__(
+        self,
+        joint_to_gear_ratios: Mapping[str, float],
+        joint_to_input_ranges: Mapping[str, tuple[float, float]],
+        mujoco_mappings: MujocoMappings,
+    ) -> None:
+        """Creates an instance of ScaledTorqueActuators."""
+        self.gear_ratios = jnp.array(
+            [joint_to_gear_ratios[m] for m in mujoco_mappings.ctrl_name_to_idx]
+        )
+        self.input_ranges = jnp.array(
+            [joint_to_input_ranges[m] for m in mujoco_mappings.ctrl_name_to_idx]
+        )
+
+    def get_ctrl(self, mjx_data: mjx.Data, action: Array) -> Array:
+        """Get the control signal from the (position) action vector."""
+        # Copy legacy and brax setup here
+        action_min = self.input_ranges[:, 0]
+        action_max = self.input_ranges[:, 1]
+        # Scale action from [-1,1] to actuator limits
+        ctrl = action * (action_max - action_min) * 0.5
         return ctrl
