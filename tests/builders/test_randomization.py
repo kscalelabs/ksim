598f3b3f8a53ac509632cb9b05ba4e7a33dc9f2e
budzianowski <budzianowski@gmail.com>
2025-03-17 17:30:35 -0700

    use real model for tests

--- tests/builders/test_randomization.py
+++ tests/builders/test_randomization.py
@@ -1,85 +1,94 @@
-"""Tests for randomization classes in the ksim package."""
+"""Tests for randomization classes."""
 
 import jax
 import jax.numpy as jnp
+import mujoco
 import pytest
+from mujoco import mjx
 
-from ksim.env.types import PhysicsModel
+from ksim.env.data import PhysicsData, PhysicsModel
 from ksim.randomization import (
-    ArmatureRandomization,
-    FloorFrictionRandomization,
-    JointDampingRandomization,
-    LinkMassRandomization,
-    StaticFrictionRandomization,
-    TorsoMassRandomization,
+    ArmatureRandomizer,
+    FloorFrictionRandomizer,
+    JointDampingRandomizer,
+    LinkMassRandomizer,
+    StaticFrictionRandomizer,
+    TorsoMassRandomizer,
 )
 
 
-class DummyPhysicsModel:
-    """Mock mjx.Model for testing."""
+def get_dummy_model() -> mjx.Model:
+    """Get a dummy mjx.Model for testing."""
+    mj_model = mujoco.MjModel.from_xml_path("tests/fixed_assets/default_humanoid_test.mjcf")
+    mjx_model = mjx.put_model(mj_model)
+    return mjx_model
+
+
+class DummyPhysicsData:
+    """Mock mjx.Data for testing."""
 
     def __init__(self) -> None:
-        self.dof_frictionloss = jnp.ones((12,))
-        self.dof_armature = jnp.ones((12,))
-        self.body_mass = jnp.ones((10,))
-        self.geom_friction = jnp.ones((10, 1))
-        self.dof_damping = jnp.ones((12,))
-        self.nq = 12
-        self.nbody = 10
+        self.qpos = jnp.ones((12,))
+        self.qvel = jnp.ones((12,))
 
 
 def test_randomization_name() -> None:
     """Test that the randomization name is correctly generated."""
-    randomizer = StaticFrictionRandomization()
-    assert randomizer.randomization_name == "dof_frictionloss"
+    randomizer = StaticFrictionRandomizer()
+    assert randomizer.name == "dof_frictionloss"
 
 
 class TestRandomizers:
     """Tests for the randomization classes."""
 
     @pytest.fixture
-    def model(self) -> PhysicsModel:
-        """Return a dummy PhysicsModel."""
-        return DummyPhysicsModel()
+    def model(self) -> mjx.Model:
+        """Return a dummy mjx.Model."""
+        return get_dummy_model()
+
+    @pytest.fixture
+    def data(self) -> mjx.Data:
+        """Return a dummy mjx.Data."""
+        return DummyPhysicsData()
 
     @pytest.fixture
     def rng(self) -> jax.Array:
         """Return a random number generator key."""
         return jax.random.PRNGKey(44)
 
-    def test_static_friction_randomization(self, model: PhysicsModel, rng: jax.Array) -> None:
-        randomizer = StaticFrictionRandomization()
-        result = randomizer(model, rng)
-        assert result.shape == model.dof_frictionloss.shape
-        assert not jnp.allclose(result, model.dof_frictionloss)
+    def test_static_friction_randomization(self, model: PhysicsModel, data: PhysicsData, rng: jax.Array) -> None:
+        randomizer = StaticFrictionRandomizer()
+        new_model, data = randomizer(model, data, rng)
+        assert new_model.dof_frictionloss.shape == model.dof_frictionloss.shape
+        assert not jnp.allclose(new_model.dof_frictionloss, model.dof_frictionloss)
 
-    def test_armature_randomization(self, model: PhysicsModel, rng: jax.Array) -> None:
-        randomizer = ArmatureRandomization()
-        result = randomizer(model, rng)
-        assert result.shape == model.dof_armature.shape
-        assert not jnp.allclose(result, model.dof_armature)
+    def test_armature_randomization(self, model: PhysicsModel, data: PhysicsData, rng: jax.Array) -> None:
+        randomizer = ArmatureRandomizer()
+        new_model, data = randomizer(model, data, rng)
+        assert new_model.dof_armature.shape == model.dof_armature.shape
+        assert not jnp.allclose(new_model.dof_armature, model.dof_armature)
 
-    def test_link_mass_randomization(self, model: PhysicsModel, rng: jax.Array) -> None:
-        randomizer = LinkMassRandomization()
-        result = randomizer(model, rng)
-        assert result.shape == model.body_mass.shape
-        assert not jnp.allclose(result, model.body_mass)
+    def test_link_mass_randomization(self, model: PhysicsModel, data: PhysicsData, rng: jax.Array) -> None:
+        randomizer = LinkMassRandomizer()
+        new_model, data = randomizer(model, data, rng)
+        assert new_model.body_mass.shape == model.body_mass.shape
+        assert not jnp.allclose(new_model.body_mass, model.body_mass)
 
-    def test_torso_mass_randomization(self, model: PhysicsModel, rng: jax.Array) -> None:
-        randomizer = TorsoMassRandomization(torso_body_id=0)
-        result = randomizer(model, rng)
-        assert result.shape == model.body_mass.shape
-        assert result[0] != model.body_mass[0]
-        assert jnp.allclose(result[1:], model.body_mass[1:])
+    def test_torso_mass_randomization(self, model: PhysicsModel, data: PhysicsData, rng: jax.Array) -> None:
+        randomizer = TorsoMassRandomizer(torso_body_id=0)
+        new_model, data = randomizer(model, data, rng)
+        assert new_model.body_mass.shape == model.body_mass.shape
+        assert new_model.body_mass[0] != model.body_mass[0]
+        assert jnp.allclose(new_model.body_mass[1:], model.body_mass[1:])
 
-    def test_floor_friction_randomization(self, model: PhysicsModel, rng: jax.Array) -> None:
-        randomizer = FloorFrictionRandomization(floor_body_id=0)
-        result = randomizer(model, rng)
-        assert result.shape == model.geom_friction.shape
-        assert not jnp.allclose(result, model.geom_friction)
+    def test_floor_friction_randomization(self, model: PhysicsModel, data: PhysicsData, rng: jax.Array) -> None:
+        randomizer = FloorFrictionRandomizer(floor_body_id=0)
+        new_model, data = randomizer(model, data, rng)
+        assert new_model.geom_friction.shape == model.geom_friction.shape
+        assert not jnp.allclose(new_model.geom_friction, model.geom_friction)
 
-    def test_joint_damping_randomization(self, model: PhysicsModel, rng: jax.Array) -> None:
-        randomizer = JointDampingRandomization()
-        result = randomizer(model, rng)
-        assert result.shape == model.dof_damping.shape
-        assert not jnp.allclose(result, model.dof_damping)
+    def test_joint_damping_randomization(self, model: PhysicsModel, data: PhysicsData, rng: jax.Array) -> None:
+        randomizer = JointDampingRandomizer()
+        new_model, data = randomizer(model, data, rng)
+        assert new_model.dof_damping.shape == model.dof_damping.shape
+        assert not jnp.allclose(new_model.dof_damping, model.dof_damping)
