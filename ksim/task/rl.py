df3920be853a9079eaa8509def8697272d927538
Benjamin Bolte <ben@bolte.cc>
2025-03-24 02:28:11 -0700

    remove valid thing that i added

--- ksim/task/rl.py
+++ ksim/task/rl.py
@@ -216,10 +216,6 @@ class RLConfig(xax.Config):
         value=1,
         help="The number of epochs between logging steps.",
     )
-    valid_every_n_seconds: float | None = xax.field(
-        value=60.0,
-        help="Run validation every N seconds",
-    )
 
     # Training parameters.
     num_envs: int = xax.field(
