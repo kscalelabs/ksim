8be983b2cf990a8a94ed23efb617d7a6ee27fb27
Benjamin Bolte <ben@bolte.cc>
2025-03-24 20:07:51 -0700

    reduce lstm rollout length

--- examples/default_humanoid/walking_lstm.py
+++ examples/default_humanoid/walking_lstm.py
@@ -356,7 +356,7 @@ if __name__ == "__main__":
             ctrl_dt=0.02,
             max_action_latency=0.0,
             min_action_latency=0.0,
-            rollout_length_seconds=21.0,
+            rollout_length_seconds=10.0,  # This needs to be shorter because of memory constraints.
             eval_rollout_length_seconds=4.0,
             # PPO parameters
             gamma=0.97,
