444dc9f48cbd1cb59766e8cb700ad769b744fe80
Benjamin Bolte <ben@bolte.cc>
2025-03-24 18:07:44 -0700

    it is inverted again

--- examples/default_humanoid/walking.py
+++ examples/default_humanoid/walking.py
@@ -59,11 +59,11 @@ class DHJointPositionObservation(ksim.Observation):
 class DHForwardReward(ksim.Reward):
     """Incentives forward movement."""
 
-    velocity_clip: float = attrs.field(default=1.0)
+    velocity_clip: float = attrs.field(default=3.0)
 
     def __call__(self, trajectory: ksim.Trajectory) -> Array:
-        # Take just the x velocity component.
-        x_delta = -jnp.clip(trajectory.qvel[..., 0], -self.velocity_clip, self.velocity_clip)
+        # Just try to maximize the velocity in the X direction.
+        x_delta = jnp.clip(trajectory.qvel[..., 0], -self.velocity_clip, self.velocity_clip)
         return x_delta
 
 
