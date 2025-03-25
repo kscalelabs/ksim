16298af7a91c83b703991f9128b7debc024a33a5
Benjamin Bolte <ben@bolte.cc>
2025-03-25 13:51:32 -0700

    remove from default example

--- examples/default_humanoid/walking.py
+++ examples/default_humanoid/walking.py
@@ -326,9 +326,6 @@ class HumanoidWalkingTask(ksim.PPOTask[Config], Generic[Config]):
             # robot to walk smoothly without feet slamming into the ground
             # (which can cause physical damage).
             ksim.BaseJerkZPenalty(scale=-0.01, ctrl_dt=self.config.ctrl_dt),
-            # This penalty incentivies the actuators to change acceleration
-            # smoothly - visually, this will look much more natural.
-            ksim.ActuatorJerkPenalty(scale=-0.01, ctrl_dt=self.config.ctrl_dt),
         ]
 
     def get_terminations(self, physics_model: ksim.PhysicsModel) -> list[ksim.Termination]:
