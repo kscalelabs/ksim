53b09804f76a7afbe031b86856880f8b364870c4
Wesley Maa <wesley.maa@gmail.com>
2025-03-25 14:27:35 -0700

    add rollout variables and bump version to 0.0.6

--- ksim/__init__.py
+++ ksim/__init__.py
@@ -1,6 +1,6 @@
 """Defines the main ksim API."""
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 from .actuators import *
 from .commands import *
--- ksim/types.py
+++ ksim/types.py
@@ -6,6 +6,7 @@ __all__ = [
     "PhysicsState",
     "Trajectory",
     "Rewards",
+    "RolloutVariables",
     "Histogram",
     "Metrics",
 ]
