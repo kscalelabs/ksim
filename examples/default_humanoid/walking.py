fcffcf75ef4d0b7935cb4d01c8432edf189732f2
Benjamin Bolte <ben@bolte.cc>
2025-04-02 14:31:06 -0700

    asymmetric distribution for modeling action space

--- examples/default_humanoid/walking.py
+++ examples/default_humanoid/walking.py
@@ -21,6 +21,30 @@ import ksim
 
 NUM_JOINTS = 21
 
+ACTION_RANGES = [
+    [-0.7853981633974483, 0.7853981633974483],
+    [-1.3089969389957472, 0.5235987755982988],
+    [-0.6108652381980153, 0.6108652381980153],
+    [-0.5235987755982988, 0.17453292519943295],
+    [-1.0471975511965976, 0.6108652381980153],
+    [-2.6179938779914944, 0.3490658503988659],
+    [-2.792526803190927, 0.03490658503988659],
+    [-0.8726646259971648, 0.8726646259971648],
+    [-0.8726646259971648, 0.8726646259971648],
+    [-0.5235987755982988, 0.17453292519943295],
+    [-1.0471975511965976, 0.6108652381980153],
+    [-2.6179938779914944, 0.3490658503988659],
+    [-2.792526803190927, 0.03490658503988659],
+    [-0.8726646259971648, 0.8726646259971648],
+    [-0.8726646259971648, 0.8726646259971648],
+    [-1.4835298641951802, 1.0471975511965976],
+    [-1.4835298641951802, 1.0471975511965976],
+    [-1.7453292519943295, 0.8726646259971648],
+    [-1.4835298641951802, 1.0471975511965976],
+    [-1.4835298641951802, 1.0471975511965976],
+    [-1.7453292519943295, 0.8726646259971648],
+]
+
 
 @attrs.define(frozen=True, kw_only=True)
 class NaiveForwardReward(ksim.Reward):
@@ -63,6 +87,7 @@ class DefaultHumanoidActor(eqx.Module):
             key=key,
             activation=jax.nn.relu,
         )
+        self.action_ranges = jnp.array(ACTION_RANGES)
         self.min_std = min_std
         self.max_std = max_std
         self.var_scale = var_scale
@@ -97,11 +122,14 @@ class DefaultHumanoidActor(eqx.Module):
 
         dist = distrax.Normal(mean_n, std_n)
         dist = distrax.Transformed(dist, distrax.Tanh())
-        # For the default humanoid, the actions are torques in the range [-1, 1],
-        # which we can model as using a Tanh distribution. However, for different
-        # action spaces, it is probably a good idea to apply a scale / shift
-        # to better model the action space.
-        # dist = distrax.Transformed(dist, distrax.ScalarAffine(self.shift, self.scale))
+
+        # In order for the neural network to model the action space effectively,
+        # we apply a bijector to the Tanh distribution mapping the support
+        # from [-1, 1] to [-min, max], centered at zero.
+        action_min, action_max = self.action_ranges[..., 0], self.action_ranges[..., 1]
+        dist = distrax.Transformed(dist, distrax.ScalarAffine(shift=jnp.zeros_like(action_min), scale=action_max))
+        dist = distrax.Transformed(dist, ksim.AsymmetricBijector(min=action_min, max=action_max))
+
         return dist
 
 
@@ -286,7 +314,10 @@ class HumanoidWalkingTask(ksim.PPOTask[Config], Generic[Config]):
         physics_model: ksim.PhysicsModel,
         metadata: dict[str, JointMetadataOutput] | None = None,
     ) -> ksim.Actuators:
-        return ksim.TorqueActuators()
+        return ksim.MITPositionActuators(
+            physics_model=physics_model,
+            joint_name_to_metadata=metadata,
+        )
 
     def get_randomization(self, physics_model: ksim.PhysicsModel) -> list[ksim.Randomization]:
         return [
---
+++ ksim/distributions.py
@@ -0,0 +1,58 @@
+"""Defines some additional useful Distrax distributions."""
+
+__all__ = [
+    "AsymmetricBijector",
+]
+
+from typing import Tuple
+
+import distrax
+import jax.numpy as jnp
+from distrax._src.utils import conversion
+from jaxtyping import Array
+
+
+class AsymmetricBijector(distrax.Bijector):
+    """A bijector that makes a distribution asymmetric.
+
+    This maps a distribution with support `[-max, max]` to a distribution with
+    support `[-min, max]`. Alternativly, this can be parametrized by providing
+    `scale = min / max`.
+    """
+
+    def __init__(
+        self,
+        min: Array | None = None,
+        max: Array | None = None,
+        scale: Array | None = None,
+    ) -> None:
+        if scale is None:
+            assert min is not None and max is not None, "If scale is not provided, min and max must be provided."
+            scale = min / max
+        else:
+            assert min is None and max is None, "If scale is provided, min and max must not be provided."
+
+        super().__init__(event_ndims_in=0)
+        self._scale = conversion.as_float_array(scale)
+
+    @property
+    def scale(self) -> Array:
+        return self._scale
+
+    def forward_log_det_jacobian(self, x: Array) -> Array:
+        """Computes log|det J(f)(x)|."""
+        return jnp.where(x < 0, jnp.log(self._scale), 0.0)
+
+    def forward_and_log_det(self, x: Array) -> Tuple[Array, Array]:
+        """Computes y = f(x) and log|det J(f)(x)|."""
+        y = jnp.where(x < 0, x * self._scale, x)
+        return y, self.forward_log_det_jacobian(x)
+
+    def inverse_and_log_det(self, y: Array) -> Tuple[Array, Array]:
+        """Computes x = f^{-1}(y) and log|det J(f^{-1})(y)|."""
+        x = jnp.where(y < 0, y / self._scale, y)
+        return x, -self.forward_log_det_jacobian(x)
+
+    def same_as(self, other: distrax.Bijector) -> bool:
+        """Returns True if this bijector is guaranteed to be the same as `other`."""
+        return type(other) is AsymmetricBijector and jnp.array_equal(self._scale, other._scale).all().item()
