a26738b427d0777ef27c32ca27a2e76e6755a828
Benjamin Bolte <ben@bolte.cc>
2025-04-06 11:56:02 -0700

    fix termination metric

--- ksim/task/rl.py
+++ ksim/task/rl.py
@@ -1013,19 +1013,21 @@ class RLTask(xax.Task[Config], Generic[Config], ABC):
         Args:
             trajectories: The trajectories to get the termination metrics for.
         """
-        termination_timesteps_selected = jnp.where(trajectories.done, trajectories.timestep, 0.0)
-        total_termination_time = jnp.sum(termination_timesteps_selected)
-        total_terminations = trajectories.done.sum()
-        episode_length = jnp.where(
-            total_terminations > 0,
-            total_termination_time / total_terminations,
-            0.0,
-        )
+        # Compute the episode length from the timesteps. The maximum episode
+        # length will plateau at the number of timesteps in the rollout.
+        timestep = trajectories.timestep
+        done_mask = trajectories.done.at[..., -1].set(True)
+        termination_sum = jnp.sum(jnp.where(done_mask, timestep, 0.0), axis=-1) - timestep[..., 0]
+        episode_length = (termination_sum / (done_mask.sum(axis=-1) + 1)).mean()
+
+        # Compute the mean number of terminations per episode, broken down by
+        # the type of termination.
         kvs = list(trajectories.termination_components.items())
         all_terminations = jnp.stack([v for _, v in kvs], axis=-1)
         has_termination = (all_terminations.any(axis=-1)).sum(axis=-1)
         num_terminations = has_termination.sum().clip(min=1)
         mean_terminations = trajectories.done.sum(-1).mean()
+
         return {
             "episode_length": episode_length,
             "mean_terminations": mean_terminations,
