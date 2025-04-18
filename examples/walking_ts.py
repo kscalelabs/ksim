09c0648e0b7d207bf5b5d8c9eca6a8e2250589c2
Benjamin Bolte <ben@bolte.cc>
2025-04-18 12:22:48 -0400

    minor updates

--- examples/walking_ts.py
+++ examples/walking_ts.py
@@ -6,18 +6,57 @@ To train this task, first you should train a walking policy using the vanilla
 
 from dataclasses import dataclass
 
+import xax
+from jaxtyping import PRNGKeyArray
+from omegaconf import MISSING
+
 import ksim
 
-from .walking import HumanoidWalkingTask, HumanoidWalkingTaskConfig
+from .walking import DefaultHumanoidModel, HumanoidWalkingTask, HumanoidWalkingTaskConfig
+
+
+class TeacherStudentModel(DefaultHumanoidModel):
+    teacher: DefaultHumanoidModel
+
+    def __init__(
+        self,
+        key: PRNGKeyArray,
+        *,
+        hidden_size: int,
+        depth: int,
+        num_mixtures: int,
+    ) -> None:
+        super().__init__(
+            key=key,
+            hidden_size=hidden_size,
+            depth=depth,
+            num_mixtures=num_mixtures,
+        )
+
+        self.teacher = DefaultHumanoidModel(
+            key=key,
+            hidden_size=hidden_size,
+            depth=depth,
+            num_mixtures=num_mixtures,
+        )
 
 
 @dataclass
 class HumanoidWalkingTeacherStudentTaskConfig(HumanoidWalkingTaskConfig):
-    pass
+    ckpt_path: str = xax.field(
+        value=MISSING,
+        help="Path to the checkpoint to load the teacher policy from.",
+    )
 
 
 class HumanoidWalkingTeacherStudentTask(
     HumanoidWalkingTask[HumanoidWalkingTeacherStudentTaskConfig],
     ksim.TeacherStudentTask[HumanoidWalkingTeacherStudentTaskConfig],
 ):
-    pass
+    def get_model(self, key: PRNGKeyArray) -> TeacherStudentModel:
+        return TeacherStudentModel(
+            key,
+            hidden_size=self.config.hidden_size,
+            depth=self.config.depth,
+            num_mixtures=self.config.num_mixtures,
+        )
