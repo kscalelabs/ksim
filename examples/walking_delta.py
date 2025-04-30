752e8e008b7abf915c538d9f4e0d9ebcfaad652a
Wesley Maa <wesley.maa@gmail.com>
2025-04-30 12:30:23 -0700

    delta task

---
+++ examples/walking_delta.py
@@ -0,0 +1,58 @@
+# mypy: disable-error-code="override"
+"""Defines simple task for training a walking policy for the default humanoid with delta position control."""
+
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+from kscale.web.gen.api import JointMetadataOutput
+
+import ksim
+
+from .walking import (
+    HumanoidWalkingTask,
+    HumanoidWalkingTaskConfig,
+)
+
+
+@dataclass
+class HumanoidWalkingDeltaTaskConfig(HumanoidWalkingTaskConfig):
+    pass
+
+
+Config = TypeVar("Config", bound=HumanoidWalkingDeltaTaskConfig)
+
+
+class HumanoidWalkingDeltaTask(HumanoidWalkingTask[Config], Generic[Config]):
+    def get_actuators(
+        self,
+        physics_model: ksim.PhysicsModel,
+        metadata: dict[str, JointMetadataOutput] | None = None,
+    ) -> ksim.Actuators:
+        assert metadata is not None, "Metadata is required"
+        return ksim.MITDeltaPositionActuators(
+            physics_model=physics_model,
+            joint_name_to_metadata=metadata,
+        )
+
+
+if __name__ == "__main__":
+    # To run training, use the following command:
+    #   python -m examples.walking_delta
+    # To visualize the environment, use the following command:
+    #   python -m examples.walking_delta run_model_viewer=True
+    HumanoidWalkingDeltaTask.launch(
+        HumanoidWalkingDeltaTaskConfig(
+            # Training parameters.
+            num_envs=2048,
+            batch_size=256,
+            num_passes=2,
+            epochs_per_log_step=1,
+            rollout_length_seconds=8.0,
+            # Simulation parameters.
+            dt=0.002,
+            ctrl_dt=0.02,
+            iterations=3,
+            ls_iterations=5,
+            max_action_latency=0.01,
+        ),
+    )
