625b5931593444ef29ce3b337aff41b2a3bdecc0
Wesley Maa <wesley.maa@gmail.com>
2025-05-21 21:21:52 -0700

    draft teacher student

--- ksim/task/teacher.py
+++ ksim/task/teacher.py
@@ -1,46 +1,35 @@
 """Defines a teacher mixin for tasks."""
 
 __all__ = [
-    "TeacherMixin",
+    "StudentTask",
+    "save_teacher",
+    "TeacherConfig",
+    "TeacherReward",
 ]
 
-import bdb
-import itertools
 import logging
-from pathlib import Path
-import time
+import os
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, replace
-from typing import Generic, Iterable, TypeVar
+from pathlib import Path
+from typing import Generic, TypeVar
 
 import attrs
-import chex
 import distrax
 import equinox as eqx
 import jax
 import jax.numpy as jnp
-import mujoco
-import numpy as np
-import optax
-import tqdm
 import xax
-from jaxtyping import Array, PRNGKeyArray, PyTree
-from omegaconf import DictConfig, OmegaConf
+from jaxtyping import Array, PyTree
 
 from ksim.rewards import Reward
-from ksim.task.ppo import PPOConfig, PPOTask, PPOVariables
+from ksim.task.ppo import PPOConfig, PPOTask
 from ksim.task.rl import (
-    LoggedTrajectory,
-    RLTask,
-    RewardState,
-    RLLoopCarry,
-    RLLoopConstants,
     RolloutConstants,
     RolloutEnvState,
     RolloutSharedState,
-    get_viewer,
 )
-from ksim.types import PhysicsModel, Trajectory
+from ksim.types import Trajectory
 
 TEACHER_OUTPUT_KEY = "_teacher_output"
 
@@ -61,12 +50,6 @@ class TeacherConfig(PPOConfig):
 
 Config = TypeVar("Config", bound=TeacherConfig)
 
-def save_teacher(task: PPOTask[Config]) -> None:
-    """Save the teacher model to a path."""
-    arrays, statics = task.teacher_arrays, task.teacher_statics
-    eqx.tree_serialise_leaves(task.exp_dir / "teacher_policy" / "arrays.eqx", arrays)
-    eqx.tree_serialise_leaves(task.exp_dir / "teacher_policy" / "statics.eqx", statics)
-
 
 @attrs.define(frozen=True, kw_only=True)
 class TeacherReward(Reward):
@@ -75,7 +58,8 @@ class TeacherReward(Reward):
     def get_reward(self, trajectory: Trajectory) -> Array:
         if trajectory.aux_outputs is None or TEACHER_OUTPUT_KEY not in trajectory.aux_outputs:
             raise ValueError(
-                "TeacherReward auxiliary output is missing! Make sure you are using it within the context of a teacher-student task, "
+                "TeacherReward auxiliary output is missing! Make sure you are using it "
+                "within the context of a teacher-student task, "
                 "which populates the auxiliary output for you."
             )
 
@@ -84,12 +68,10 @@ class TeacherReward(Reward):
 
         assert isinstance(teacher_dist, distrax.Distribution)
 
-        # compute the log probability of the student action under the teacher distribution
         log_prob = teacher_dist.log_prob(student_action)
-
-        # reward high log probability actions
-        reward = jnp.sum(log_prob, axis=-1)
-        return reward
+        log_prob_max = teacher_dist.log_prob(teacher_dist.mode())  # value at mode
+        reward = jnp.exp(log_prob - log_prob_max)  # 1.0 at the teacher mean
+        return reward.mean(axis=-1)
 
 
 class StudentTask(PPOTask[Config], Generic[Config], ABC):
@@ -98,20 +80,36 @@ class StudentTask(PPOTask[Config], Generic[Config], ABC):
     This task extends PPO to include teacher-student training.
     """
 
-    teacher_arrays: tuple[PyTree, ...] | None = None
-    teacher_statics: tuple[PyTree, ...] | None = None
+    _teacher_policy: PyTree | None = None
 
-    def load_teacher(self, path: Path) -> None:
+    def run(self) -> None:
+        """Load the teacher model before running the task."""
+        self.load_teacher(Path(self.config.teacher_model_path), self.get_teacher_template())
+        super().run()
+
+    @abstractmethod
+    def get_teacher_template(self) -> PyTree:
+        """Get the teacher model template."""
+        raise NotImplementedError("Subclasses must implement this method.")
+
+    @abstractmethod
+    def run_teacher(self, obs: xax.FrozenDict[str, Array], cmd: xax.FrozenDict[str, Array]) -> distrax.Distribution:
+        """Run the teacher model.
+
+        obs and cmd have shape (timesteps, ...)
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
+
+    def load_teacher(self, path: Path, like: PyTree) -> None:
         """Load the teacher model from a path."""
-        self.teacher_arrays = eqx.tree_deserialise_leaves(path / "arrays.eqx")
-        self.teacher_statics = eqx.tree_deserialise_leaves(path / "statics.eqx")
+        self._teacher_policy = eqx.tree_deserialise_leaves(path / "teacher.eqx", like=like)
 
-
+    @property
     def teacher_policy(self) -> PyTree:
-        if self.teacher_statics is None or self.teacher_arrays is None:
+        if self._teacher_policy is None:
             raise ValueError("Teacher model not loaded! Call load_teacher() first.")
 
-        return eqx.combine(self.teacher_arrays, self.teacher_statics)
+        return self._teacher_policy
 
     def postprocess_trajectory(
         self,
@@ -121,16 +119,13 @@ class StudentTask(PPOTask[Config], Generic[Config], ABC):
         trajectory: Trajectory,
     ) -> Trajectory:
         trajectory = super().postprocess_trajectory(
-                constants=constants,
-                env_states=env_states,
+            constants=constants,
+            env_states=env_states,
             shared_state=shared_state,
+            trajectory=trajectory,
         )
 
-        teacher_policy = self.teacher_policy()
-
-        action_dist = teacher_policy.run_teacher(trajectory.obs)
-
-        chex.assert_shape(action_dist, (trajectory.num_steps, *trajectory.action_shape))
+        action_dist = self.run_teacher(trajectory.obs, trajectory.command)
 
         # adds the teacher distribution to the aux outputs
         aux_outputs = trajectory.aux_outputs.unfreeze() if trajectory.aux_outputs else {}
@@ -138,3 +133,9 @@ class StudentTask(PPOTask[Config], Generic[Config], ABC):
         trajectory = replace(trajectory, aux_outputs=xax.FrozenDict(aux_outputs))
 
         return trajectory
+
+
+def save_teacher(policy: PyTree, path: Path) -> None:
+    """Save the teacher model to a path."""
+    os.makedirs(path, exist_ok=True)
+    eqx.tree_serialise_leaves(path / "teacher.eqx", policy)
