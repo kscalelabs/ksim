19a681eb892a2c2f661e5754621f6096077953ce
Ben Bolte <ben@bolte.cc>
2025-06-26 15:57:16 -0700

    fix amp noise dimensions (#446)

--- ksim/task/amp.py
+++ ksim/task/amp.py
@@ -389,16 +389,16 @@ class AMPTask(PPOTask[Config], Generic[Config], ABC):
         sim_rng, real_rng = jax.random.split(rng)
         max_noise = self.config.amp_reference_noise
         min_noise = max_noise * self.config.amp_reference_noise_min_multiplier
-        cur_level = carry.env_states.curriculum_state.level
+        cur_level = carry.env_states.curriculum_state.level.mean()
         noise_level = max_noise - (max_noise - min_noise) * cur_level
         sim_motions = sim_motions + jax.random.normal(sim_rng, sim_motions.shape) * noise_level
         real_motions = real_motions + jax.random.normal(real_rng, real_motions.shape) * noise_level
 
         # Computes the discriminator loss.
-        disc_fn = xax.vmap(self.call_discriminator, in_axes=(None, 0), jit_level=JitLevel.RL_CORE)
+        disc_fn = xax.vmap(self.call_discriminator, in_axes=(None, 0, 0), jit_level=JitLevel.RL_CORE)
         real_disc_rng, sim_disc_rng = jax.random.split(rng)
-        real_disc_logits = disc_fn(model, real_motions, real_disc_rng)
-        sim_disc_logits = disc_fn(model, sim_motions, sim_disc_rng)
+        real_disc_logits = disc_fn(model, real_motions, jax.random.split(real_disc_rng, real_motions.shape[0]))
+        sim_disc_logits = disc_fn(model, sim_motions, jax.random.split(sim_disc_rng, sim_motions.shape[0]))
         real_disc_loss, sim_disc_loss = self.get_disc_losses(real_disc_logits, sim_disc_logits)
 
         disc_loss = real_disc_loss + sim_disc_loss
