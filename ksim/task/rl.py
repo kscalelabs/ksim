a80b8ff1483c94de3ca54bec21ed65ae0391b009
Benjamin Bolte <ben@bolte.cc>
2025-07-19 03:04:02 -0400

    nans

--- ksim/task/rl.py
+++ ksim/task/rl.py
@@ -1262,6 +1262,8 @@ class RLTask(xax.Task[Config], Generic[Config], ABC):
 
                     if processed_value.shape[1] > 1:
                         ax.legend()
+                    if (nan_cnt := np.isnan(processed_value).sum()) > 0:
+                        key = f"{key} ({nan_cnt} nan)"
                     ax.set_title(key)
 
                 # Create and log the image
