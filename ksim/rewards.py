c967ecb540fe7bc90160e102b03df0a325316b02
Benjamin Bolte <ben@bolte.cc>
2025-08-17 14:39:02 -0700

    SymmetryReward

--- ksim/rewards.py
+++ ksim/rewards.py
@@ -25,6 +25,7 @@ __all__ = [
     "UprightReward",
     "LinkAccelerationPenalty",
     "LinkJerkPenalty",
+    "SymmetryReward",
     "ReachabilityPenalty",
     "FeetAirTimeReward",
     "FeetHeightReward",
