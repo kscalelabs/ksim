3513314ccd02d08147a440ebebae31bf552c6a1a
Benjamin Bolte <ben@bolte.cc>
2025-08-24 00:55:42 -0400

    more refactoring

--- ksim/actuators.py
+++ ksim/actuators.py
@@ -10,38 +10,24 @@ __all__ = [
 
 import logging
 from abc import ABC, abstractmethod
-from typing import Literal
 
 import chex
 import jax
 import jax.numpy as jnp
 from jaxtyping import Array, PRNGKeyArray, PyTree
 
+from ksim.noise import Noise, NoNoise
 from ksim.types import Metadata, PhysicsData, PhysicsModel
 from ksim.utils.mujoco import get_ctrl_data_idx_by_name
 
 logger = logging.getLogger(__name__)
 
-NoiseType = Literal["none", "uniform", "gaussian"]
-
 
 class Actuators(ABC):
     """Collection of actuators."""
 
-    @classmethod
-    def add_noise(cls, noise: float, noise_type: NoiseType, action: Array, rng: PRNGKeyArray) -> Array:
-        match noise_type:
-            case "none":
-                return action
-            case "uniform":
-                return action + (jax.random.uniform(rng, action.shape) * 2 - 1) * noise
-            case "gaussian":
-                return action + jax.random.normal(rng, action.shape) * noise
-            case _:
-                raise ValueError(f"Invalid noise type: {noise_type}")
-
     @abstractmethod
-    def get_ctrl(self, action: Array, physics_data: PhysicsData, rng: PRNGKeyArray) -> Array:
+    def get_ctrl(self, action: Array, physics_data: PhysicsData, curriculum_level: Array, rng: PRNGKeyArray) -> Array:
         """Get the control signal from the action vector."""
 
     def get_default_action(self, physics_data: PhysicsData) -> Array:
@@ -64,22 +50,21 @@ class StatefulActuators(Actuators):
     def get_initial_state(self, physics_data: PhysicsData, rng: PRNGKeyArray) -> PyTree:
         """Get the initial state for the actuator."""
 
-    def get_ctrl(self, action: Array, physics_data: PhysicsData, rng: PRNGKeyArray) -> Array:
+    def get_ctrl(self, action: Array, physics_data: PhysicsData, curriculum_level: Array, rng: PRNGKeyArray) -> Array:
         raise NotImplementedError("Stateful actuators should use `get_stateful_ctrl` instead.")
 
 
 class TorqueActuators(Actuators):
     """Direct torque control."""
 
-    def __init__(self, noise: float = 0.0, noise_type: NoiseType = "none") -> None:
+    def __init__(self, noise: Noise | None = None) -> None:
         super().__init__()
 
-        self.noise = noise
-        self.noise_type: NoiseType = noise_type
+        self.noise = NoNoise() if noise is None else noise
 
-    def get_ctrl(self, action: Array, physics_data: PhysicsData, rng: PRNGKeyArray) -> Array:
+    def get_ctrl(self, action: Array, physics_data: PhysicsData, curriculum_level: Array, rng: PRNGKeyArray) -> Array:
         """Just use the action as the torque, the simplest actuator model."""
-        return self.add_noise(self.noise, self.noise_type, action, rng)
+        return self.noise.add_noise(action, curriculum_level, rng)
 
 
 class PositionActuators(Actuators):
@@ -89,10 +74,8 @@ class PositionActuators(Actuators):
         self,
         physics_model: PhysicsModel,
         metadata: Metadata,
-        action_noise: float = 0.0,
-        action_noise_type: NoiseType = "none",
-        torque_noise: float = 0.0,
-        torque_noise_type: NoiseType = "none",
+        action_noise: Noise | None = None,
+        torque_noise: Noise | None = None,
     ) -> None:
         """Creates easily vector multipliable kps and kds."""
         ctrl_name_to_idx = get_ctrl_data_idx_by_name(physics_model)
@@ -134,10 +117,8 @@ class PositionActuators(Actuators):
         self.kds = jnp.array(kds_list)
         self.ctrl_clip = jnp.array(ctrl_clip_list)
 
-        self.action_noise = action_noise
-        self.action_noise_type: NoiseType = action_noise_type
-        self.torque_noise = torque_noise
-        self.torque_noise_type: NoiseType = torque_noise_type
+        self.action_noise = NoNoise() if action_noise is None else action_noise
+        self.torque_noise = NoNoise() if torque_noise is None else torque_noise
 
         if any(self.kps < 0) or any(self.kds < 0):
             raise ValueError("Some KPs or KDs are negative. Check the provided metadata.")
@@ -148,24 +129,22 @@ class PositionActuators(Actuators):
         # This can be overridden if necessary.
         return f"{joint_name}_ctrl"
 
-    def get_ctrl(self, action: Array, physics_data: PhysicsData, rng: PRNGKeyArray) -> Array:
+    def get_ctrl(self, action: Array, physics_data: PhysicsData, curriculum_level: Array, rng: PRNGKeyArray) -> Array:
         """Get the control signal from the (position) action vector."""
         pos_rng, tor_rng = jax.random.split(rng)
         current_pos = physics_data.qpos[7:]  # First 7 are always root pos.
         current_vel = physics_data.qvel[6:]  # First 6 are always root vel.
 
-        # Clamp target positions to joint limits
-        target_velocities = jnp.zeros_like(action)
-        pos_delta = self.add_noise(self.action_noise, self.action_noise_type, action - current_pos, pos_rng)
-        vel_delta = target_velocities - current_vel
+        # Add position and velocity noise
+        target_position = self.action_noise.add_noise(action, curriculum_level, pos_rng)
+        target_velocity = jnp.zeros_like(action)
+
+        pos_delta = target_position - current_pos
+        vel_delta = target_velocity - current_vel
 
         ctrl = self.kps * pos_delta + self.kds * vel_delta
-
-        return jnp.clip(
-            self.add_noise(self.torque_noise, self.torque_noise_type, ctrl, tor_rng),
-            -self.ctrl_clip,
-            self.ctrl_clip,
-        )
+        ctrl = self.torque_noise.add_noise(ctrl, curriculum_level, tor_rng)
+        return jnp.clip(ctrl, -self.ctrl_clip, self.ctrl_clip)
 
 
 class PositionVelocityActuator(PositionActuators):
@@ -175,26 +154,20 @@ class PositionVelocityActuator(PositionActuators):
         self,
         physics_model: PhysicsModel,
         metadata: Metadata,
-        pos_action_noise: float = 0.0,
-        pos_action_noise_type: NoiseType = "none",
-        vel_action_noise: float = 0.0,
-        vel_action_noise_type: NoiseType = "none",
-        torque_noise: float = 0.0,
-        torque_noise_type: NoiseType = "none",
+        pos_action_noise: Noise | None = None,
+        vel_action_noise: Noise | None = None,
+        torque_noise: Noise | None = None,
     ) -> None:
         super().__init__(
             physics_model=physics_model,
             metadata=metadata,
             action_noise=pos_action_noise,
-            action_noise_type=pos_action_noise_type,
             torque_noise=torque_noise,
-            torque_noise_type=torque_noise_type,
         )
 
-        self.vel_action_noise = vel_action_noise
-        self.vel_action_noise_type: NoiseType = vel_action_noise_type
+        self.vel_action_noise = NoNoise() if vel_action_noise is None else vel_action_noise
 
-    def get_ctrl(self, action: Array, physics_data: PhysicsData, rng: PRNGKeyArray) -> Array:
+    def get_ctrl(self, action: Array, physics_data: PhysicsData, curriculum_level: Array, rng: PRNGKeyArray) -> Array:
         """Get the control signal from the (position and velocity) action vector."""
         pos_rng, vel_rng, tor_rng = jax.random.split(rng, 3)
 
@@ -207,15 +180,15 @@ class PositionVelocityActuator(PositionActuators):
         chex.assert_equal_shape([current_pos, target_position, current_vel, target_velocity])
 
         # Add position and velocity noise
-        target_position = self.add_noise(self.action_noise, self.action_noise_type, target_position, pos_rng)
-        target_velocity = self.add_noise(self.vel_action_noise, self.vel_action_noise_type, target_velocity, vel_rng)
+        target_position = self.action_noise.add_noise(target_position, curriculum_level, pos_rng)
+        target_velocity = self.vel_action_noise.add_noise(target_velocity, curriculum_level, vel_rng)
 
         pos_delta = target_position - current_pos
         vel_delta = target_velocity - current_vel
 
         ctrl = self.kps * pos_delta + self.kds * vel_delta
         return jnp.clip(
-            self.add_noise(self.torque_noise, self.torque_noise_type, ctrl, tor_rng),
+            self.torque_noise.add_noise(ctrl, curriculum_level, tor_rng),
             -self.ctrl_clip,
             self.ctrl_clip,
         )
